5d2842f43bb8120b96e6c98b02f513852cbd47cb
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-05-13 21:37:24 -0300

    chore: delete das pasta commonjs e ESM modules

--- dio/node-ESM-modules/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-export const key = {
-    value: "42342fsz",
-    permission: "admin"
-}
-
-export const getDataFromApi = () => {
-    console.log("dados da api sendo buscados...")
-}
--- dio/node-commonJs-modules/src/services/config.js
+++
@@ -1,8 +0,0 @@
-// export const 
-
-const devArea = {
-    version: "1.0.3",
-    production: true
-}
-
-module.exports = devArea;
